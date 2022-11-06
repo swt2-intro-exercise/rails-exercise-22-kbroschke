9360e21c7ca3f03b2042587ed5f27219f2cfedea
Karl Broschke <karl.broschke@student.hpi.de>
2022-11-06 20:08:22 +0100

    Add link back to index on new_author_path And test for it

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -1,10 +1,18 @@
 require 'rails_helper'
 
 describe "New author page", type: :feature do
+
   it "should exist at 'new_author_path' and render withour error" do
     # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
     visit new_author_path
   end
+
+  it "should have a link back to authors index" do
+    # this test/feature was not required by the exercise but it was included in https://guides.rubyonrails.org/v6.0/getting_started.html#adding-links
+    visit new_author_path
+    expect(page).to have_link(href: authors_path)
+  end
+
   it "should have text input for first name, last name, and homepage" do
     visit new_author_path
     # these are the standard names given to inputs by the Rails form builder
@@ -12,6 +20,7 @@ describe "New author page", type: :feature do
     expect(page).to have_field('author[last_name]')
     expect(page).to have_field('author[homepage]')
   end
+
   it "should save a new author in the database" do
     before_count = Author.count
 
