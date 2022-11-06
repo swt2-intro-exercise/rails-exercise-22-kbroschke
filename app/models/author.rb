d06957f2d65d2595a3d898f82b83e4d85ceadef3
Karl Broschke <karl.broschke@student.hpi.de>
2022-11-06 18:19:12 +0100

    Fix var accessing, closes #3

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ApplicationRecord
-  def name()
-    @first_name + " " + @last_name
+  def name
+    first_name + " " + last_name
   end
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,21 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-  before(:each) do
-    @first_name = "Alan"
-    @last_name = "Turing"
-    @homepage = "http://wikipedia.org/Alan_Turing"
-    @author = Author.new(
-      first_name: @first_name,
-      last_name: @last_name,
-      homepage: @homepage)
-  end
-  it "should have all attributes set" do
-    expect(@author.first_name).to eq(@first_name)
-    expect(@author.last_name).to eq(@last_name)
-    expect(@author.homepage).to eq(@homepage)
-  end
-  it "should concat first and last name to full name" do
-    expect(@author.name).to eq(@first_name + " " + @last_name)
+  it "should return concatenation of first and last name when invoking 'name'" do
+    author = Author.new(first_name: "Alan", last_name: "Turing")
+    expect(author.name).to eq("Alan Turing")
   end
 end
