a4180263cf39910963d57318b0f441bdf7a70720
Karl Broschke <karl.broschke@student.hpi.de>
2022-11-06 19:56:10 +0100

    Failing test for #7

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,8 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
+
+  before(:all) do
+    @turing = create(:author)
+  end
+
   it "should return concatenation of first and last name when invoking 'name'" do
-    author = Author.new(first_name: "Alan", last_name: "Turing")
-    expect(author.name).to eq("Alan Turing")
+    expect(@turing.name).to eq("Alan Turing")
+  end
+
+  it "should be valid with valid attributes" do
+    expect(@turing).to be_valid
+  end
+
+  it "should not be valid without last name" do
+    @alan = build(:author, last_name: "")
+    expect(@alan).not_to be_valid
+  end
+
+  it "should not be valid with empty last name" do
+    @alan2 = build(:author, last_name: "")
+    expect(@alan2).not_to be_valid
   end
 end
