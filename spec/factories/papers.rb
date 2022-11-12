8aefb8fc0e68a00e8c68926e913b0755ddf07640
Karl Broschke <karl.broschke@student.hpi.de>
2022-11-13 00:06:54 +0100

    Add example with author to paper factory

--- spec/factories/papers.rb
+++ spec/factories/papers.rb
@@ -3,7 +3,11 @@ FactoryBot.define do
     title { "COMPUTING MACHINERY AND INTELLIGENCE" }
     venue { "Mind 49: 433-460" }
     year { 1950 }
-    # authors { build_list :author, 1 }
-    # not yet implemented
+    authors { build_list :author, 1 }
+  end
+  factory :authorless_paper, class: "Paper" do
+    title { "COMPUTING MACHINERY AND INTELLIGENCE" }
+    venue { "Mind 49: 433-460" }
+    year { 1950 }
   end
 end
