558ac54d01fcdb5b4ff3b78d4c8222339be65b3e
Karl Broschke <karl.broschke@student.hpi.de>
2022-11-12 23:17:46 +0100

    Failing test for #13

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:all) do
+    @paper = create(:paper)
+  end
+
+  it "should be valid with valid attributes" do
+    expect(@paper).to be_valid
+  end
+
+  it "should not be valid without title" do
+    @title_paper = build(:paper, title: "")
+    expect(@title_paper).not_to be_valid
+  end
+
+  it "should not be valid without venue" do
+    @venue_paper = build(:paper, venue: "")
+    expect(@venue_paper).not_to be_valid
+  end
+
+  it "should not be valid without year" do
+    @year_paper = build(:paper, year: "")
+    expect(@year_paper).not_to be_valid
+  end
+
+  it "should not be valid with string as year" do
+    @year_string_paper = build(:paper, year: "ABCD")
+    expect(@year_string_paper).not_to be_valid
+  end
 end
