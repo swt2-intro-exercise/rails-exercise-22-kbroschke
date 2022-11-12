fbdaa6e87bc7e9d5d6a8e7aac42fd96a28149bde
Karl Broschke <karl.broschke@student.hpi.de>
2022-11-12 23:50:46 +0100

    Failing test for #15

--- spec/features/paper/index_paper_spec.rb
+++ spec/features/paper/index_paper_spec.rb
@@ -12,10 +12,9 @@ describe "Index author page", type: :feature do
     expect(page).to have_link(href: edit_paper_path(@paper))
   end
 
-  # it "should have link to delete an author" do
-  #   visit authors_path
-  #
-  #   expect(find_all("tr")).to include(
-  #     have_link("Delete", href: author_path(@author)))
-  # end
+  it "should have link to delete a paper" do
+    visit papers_path
+
+    expect(page).to have_css("a[data-turbo-method='delete'][href='#{paper_path(@paper)}']")
+  end
 end
